c784c84ee2468c6b34bee6629463ddfe3e53f90f
isaac meira <fspp@outlook.com>
2019-04-23 12:32:43 -0300

    update indes.js (Final)

--- index.js
+++ index.js
@@ -8,8 +8,45 @@ nunjucks.configure('views', {
   watch: true
 })
 
-app.use(express.urlencoded({
-  extended: false
-}))
+app.use(
+  express.urlencoded({
+    extended: false
+  })
+)
 
 app.set('view engine', 'njk')
+
+const checkParam = (req, res, next) => {
+  const { age } = req.query
+
+  if (age === null || age === undefined) {
+    return res.redirect('/')
+  }
+  return next()
+}
+
+app.get('/', (req, res) => {
+  return res.render('age')
+})
+
+app.get('/minor', checkParam, (req, res) => {
+  const { age } = req.query
+  return res.render('minor', { age })
+})
+
+app.get('/major', checkParam, (req, res) => {
+  const { age } = req.query
+  return res.render('major', { age })
+})
+
+app.post('/check', (req, res) => {
+  const { age } = req.body
+
+  if (age >= 18) {
+    return res.redirect(`/major?age=${age}`)
+  } else {
+    return res.redirect(`/minor?age=${age}`)
+  }
+})
+
+app.listen(3000)
